fe1e023f09c6b2732f0ee1b29f53798a372ad08b
David Kwasny <dkwasny@us.ibm.com>
2021-01-13 10:18:24 -0500

    Changing base fhir url

--- _refresh.sh
+++ _refresh.sh
@@ -4,13 +4,15 @@ tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
 ig_resource_path=./input/ecqm-content-r4.xml
 
+fhir_server_base="http://localhost:8080/baseR4/";
+
 set -e
 echo Checking internet connection...
 #wget -q --spider tx.fhir.org
 
 if [ $? -eq 0 ]; then
 	echo "Online"
-	fsoption="-fs http://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/"
+	fsoption="-fs $fhir_server_base"
 else
 	echo "Offline"
 	fsoption=""
